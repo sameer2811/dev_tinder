7f05f3b0afd516801bbf8f35e6e9e9f5ea2f25a6
Sameer Singh <sameersingh@Zupee-774.local>
2024-11-08 01:24:01 +0530

    Playing with the basic routing.

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const { serverConfig } = require('./config');
 const app = express()
 
 
+// Normal middleware executed  whenever any request is hit.
 app.use(function (req, res , next) {
     console.log("I will be hit even when any request is sent");
     next();
 });
 
+// Normal Route Listening to all the request starting with /url1
 app.use('/url1', function (req, res,next) {
     console.log("Server is listening at the url1");
     // return res.json({
@@ -19,7 +21,9 @@ app.use('/url1', function (req, res,next) {
     // })
     next()
 })
-app.get('/url1/url2', function (req, res) {
+
+// Specific Get Route listening to /url1/url2
+app.get('/url1/url2', function (req, res,next) {
     console.log("Server is listening at the url2");
     return res.json({
         success: true,
@@ -27,6 +31,14 @@ app.get('/url1/url2', function (req, res) {
     })
 })
 
+// Default Middleware 
+app.use('/', function (req, res, next) {
+    console.log("Server is listening at the url root");
+    return res.json({
+        success: true,
+        msg: "Hola !! Server is up and listening at root"
+    })
+})
 
 // Function for starting the server
 function startServer() {
@@ -35,7 +47,6 @@ function startServer() {
         console.log("Server has started listening on ", process.env.PORT);
     });
 }
-
 
 // starting the server for the node.js
 startServer();
